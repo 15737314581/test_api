18a901572ca921bc7a50a7ec4565757bb56a26b2
jijianfeng <1065109432@qq.com>
2022-06-08 19:04:55 +0800

    5555

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 # coding = utf-8
 import pytest
-pytest.main(["--html=/Users/jijianfeng/Downloads/jenkins/jenkins_hone/workspace/test456/report/report.html", "--self-contained-html"])
+pytest.main(["-s",
+                 "-v",
+                 "test_api.py::TestMain::test_api",
+                 "--capture=sys",
+                 "--html=./report/report.html",
+                 "--self-contained-html"])
